e11eba2d3c3efebec2e4eb96136bc0771816bee7
andrewan <andrewan653@gmail.com>
2025-07-27 03:14:13 +0700

    tesdt

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,7 +1,7 @@
 @isset($course)
 @php
     $thumbnailUrl = $course->thumbnail !== '-' 
-        ? StorageUrl($course->thumbnail) 
+        ? SupabaseUrl::getPublicUrl($course->thumbnail) 
         : asset('images/default.jpg');
 @endphp
 
